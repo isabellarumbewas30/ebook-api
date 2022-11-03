89b9efce9bc53d8b7e873b94df48a5875c955ed2
isabellarumbewas30 <3103120113@student.smktelkom-pwt.sch.id>
2022-11-03 10:38:39 +0700

    Praktik Authentication kelar

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Public Route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/Books/{id}', [BookController::class, 'show']);
+Route::get('/authors', [AuthorController::class, 'index']);
+Route::get('/authors/{id}', [AuthorController::class, 'show']);
+
+//Protected Routes
+Route::middleware('auth:sanctum')->group(function(){
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('authors', AuthorController::class)->except('create', 'edit', 'show', 'index');
+});
+
 Route::get('/me', [AuthController::class, 'me']);
 
 // Route::get('/books', [BookController::class, 'index']);
